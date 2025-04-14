5a10a532ad109656f86a0b8e74890d0a18609e87
Harrie Stam <Harrie.stam@allroundvp.nl>
2025-04-14 20:22:15 +0200

    Tekst default route veranderd

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hello World!</p>"
+    return "<p>Leuke cursus</p>"
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
